a019c565a3690a14d652764924579c45824218a1
Hristov <mr.hristo.hristov@gmail.com>
2026-09-09 15:11:14 +0300

    Initialize paint pack registry on server startup

--- Scripts/5_Mission/PaintZ/PaintZ_MissionServer.c
+++ Scripts/5_Mission/PaintZ/PaintZ_MissionServer.c
@@ -4,6 +4,7 @@ modded class MissionServer
     {
         super.OnInit();
         PaintZ_RuntimeDocs.Refresh();
+        PaintZ_PaintPackRegistry.EnsureInitialized();
         PaintZ_PatternScaling.StartServer();
         PaintZ_ActionTuning.StartServer();
         PaintZ_ItemPolicy.StartServer();
@@ -20,10 +21,6 @@ modded class MissionServer
     override void InvokeOnConnect(PlayerBase player, PlayerIdentity identity)
     {
         super.InvokeOnConnect(player, identity);
-
-        // The player network object is not guaranteed to accept a targeted RPC
-        // from inside InvokeOnConnect itself. Defer one tick of connection setup;
-        // periodic reload broadcasts remain a later recovery path.
         GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(PaintZ_ItemPolicy.SendToClient, 1000, false, player, identity);
         GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(PaintZ_ActionTuning.SendToClient, 1000, false, player, identity);
     }
